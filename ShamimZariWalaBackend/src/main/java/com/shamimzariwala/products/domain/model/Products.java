19667f4ca2764a20a478206ed3c7a2f3041ffdf5
SHAKIR SHAMIM <motoshakir@gmail.com>
2026-05-16 21:10:12 +0530

    BE - product api inital structure

---
+++ ShamimZariWalaBackend/src/main/java/com/shamimzariwala/products/domain/model/Products.java
@@ -0,0 +1,15 @@
+package com.shamimzariwala.products.domain.model;
+
+import java.util.Date;
+
+public class Products{
+    private long id;
+    private String name;
+    private String descripton;
+    private Date createdAt;
+
+}
+
+
+
+
---
+++ ShamimZariWalaBackend/src/main/java/com/shamimzariwala/products/domain/model/ProductsInstance.java
@@ -0,0 +1,15 @@
+package com.shamimzariwala.products.domain.model;
+
+import java.util.Date;
+
+public class ProductsInstance{
+    private long id;
+    private String imageUrl;
+    private Date createdAt;
+    private Date deletedAt;
+    private int quantity;
+    private long price;
+    private String sku;
+    private long productId;
+
+}
